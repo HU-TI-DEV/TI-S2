14bd46dd4f7e5231a2314b6ecdc3eadd1525b4f9
Hagen <61312340+hagen-git@users.noreply.github.com>
2025-04-25 16:26:21 +0200

    SFML review/bijwerking (#145)
    * add PNG class diagram, show in overview, change SFML 2.0 to 2.x
    
    * almost identical main.cpp for sfml 2.x and 3.x
    
    ---------
    
    Co-authored-by: Hagen Patzke <hagen.patzke@hu.nl>
    Co-authored-by: n0harm <31340954+n0harm@users.noreply.github.com>

--- software/inrichten-ontwikkelomgeving/sfml_auto_test/README.md
+++ software/inrichten-ontwikkelomgeving/sfml_auto_test/README.md
@@ -1,5 +1,5 @@
 ## SFML voorbeeld met een auto (voertuig) classe
 
-Dit is een oefening waarbij gebruik wordt gemaakt van een auto (lees voertuig) classe. We tekenen de auto met SFML. Voorbeeld code in [main.cpp](./main.cpp). Kan je de auto compleet maken en laten bewegen? De code is voor SFML 2.0 de huidige versie van SFML is 3.0 en daar is [deze code voor aangepast](./auto_new/main.cpp).
+Dit is een oefening waarbij gebruik wordt gemaakt van een auto (lees voertuig) classe. We tekenen de auto met SFML. Voorbeeld code in [main.cpp](./main.cpp). Kan je de auto compleet maken en laten bewegen? De code is voor SFML 2.x, de huidige versie van SFML is 3.0 en daar is [deze code voor aangepast](./auto_new/main.cpp).
 
 ![class diagram](../sfml_auto_test/images/drawio_uml_class_diagram.svg)
---
+++ software/inrichten-ontwikkelomgeving/sfml_auto_test/sfml-major-versies/main-sfml-3-0-0.cpp
@@ -0,0 +1,106 @@
+// SFML 3.0.0 version
+#include <SFML/Graphics.hpp>
+#include <cmath>
+
+const float PI = 3.14159265f;
+
+class Wiel
+{
+private:
+	sf::RenderWindow& renderWindow;
+	sf::CircleShape circle;
+	sf::Vector2f position;
+
+public:
+	Wiel(int x, int y, int radius, sf::RenderWindow& renderWindow)
+		:position((float)x, (float)y), circle((float)radius), renderWindow(renderWindow)
+	{
+		// set inside color of Wiel to yellow
+		circle.setFillColor(sf::Color::Yellow);
+		// set the origin of the circle from the top left corner to its center
+		circle.setOrigin(sf::Vector2f(circle.getRadius(), circle.getRadius()));
+	}
+
+	void setPos(int xNew, int yNew) {
+		position = { (float)xNew, (float)yNew };
+	}
+
+	void draw() {
+		// set the position of the SFML-object (setPos might have changed the coordinates)
+		circle.setPosition(position);
+		renderWindow.draw(circle);
+	}
+};
+
+// TODO: add Chassis.
+
+class Auto {
+
+private:
+	Wiel voorwiel, achterwiel;
+	int x, y;
+	const int voorwielX = 140;
+	const int voorwielY = 35;
+	const int achterwielX = 40;
+	const int achterwielY = 35;
+
+public:
+	Auto(int x, int y, sf::RenderWindow& renderWindow)
+		: voorwiel(0, 0, 10, renderWindow),
+		achterwiel(0, 0, 10, renderWindow),
+		x(x), y(y)
+	{}
+
+	void setPos(int x, int y)
+	{
+		this->x = x;
+		this->y = y;
+
+		voorwiel.setPos(
+			this->x + voorwielX,
+			this->y + voorwielY);
+
+		achterwiel.setPos(
+			this->x + achterwielX,
+			this->y + achterwielY);
+	}
+
+	void draw()
+	{
+		voorwiel.draw();
+		achterwiel.draw();
+	}
+};
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Auto Class");
+
+	Auto clownsAuto(100, 500, window);
+
+	sf::Clock clock;
+
+	// main event loop: run the program as long as the window is open
+	while (window.isOpen()) {
+
+		// check all the window's events received since the last iteration of the loop
+		while (auto event = window.pollEvent()) {
+			// "close requested" event: we close the window
+			if (event->is<sf::Event::Closed>())
+				window.close();
+		} // while event
+
+		window.clear(sf::Color::Black);
+
+		// compute new position
+		float t = clock.getElapsedTime().asSeconds();
+		float horizontalOffset = 100.f * std::sin(2 * PI * t);
+		clownsAuto.setPos(100 + (int(horizontalOffset)), 500);
+
+		clownsAuto.draw();
+
+		window.display();
+	}// while window.isOpen()
+
+	return 0;
+}
