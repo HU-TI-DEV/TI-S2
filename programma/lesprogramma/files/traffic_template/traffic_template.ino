e4d36a1d108702ad08e3b9f29ae9a339a72eb15d
bbozon <bartbozon@gmail.com>
2025-05-19 07:57:31 +0200

    Latest

---
+++ programma/lesprogramma/files/traffic_template/traffic_template.ino
@@ -0,0 +1,57 @@
+enum State {
+  GREEN,
+  YELLOW_COMING_FROM_GREEN
+};
+
+class TrafficLight {
+private:
+  const int greenPin = 11;
+  const int yellowPin = 12;
+  const int buttonA = 2;
+  State currentState;
+  unsigned long lastUpdate;
+
+  void setGreen() {
+    digitalWrite(greenPin, LOW);
+    digitalWrite(yellowPin, HIGH);
+  }
+
+  void setYellow() {
+    digitalWrite(yellowPin, LOW);
+    digitalWrite(greenPin, HIGH);
+  }
+
+public:
+  TrafficLight()
+    : currentState(GREEN), lastUpdate(0) {
+    pinMode(greenPin, OUTPUT);
+    pinMode(yellowPin, OUTPUT);
+    setGreen();
+  }
+
+  void update(unsigned long now) {
+    bool buttonAPressed = !digitalRead(buttonA);
+    switch (currentState) {
+      case GREEN:
+        if (buttonAPressed) {
+          currentState = YELLOW_COMING_FROM_GREEN;
+          lastUpdate = now;
+        }
+        break;
+      case YELLOW_COMING_FROM_GREEN:
+        if (now - lastUpdate >= 2000) {
+          currentState = GREEN;
+        }
+        break;
+    }
+  }
+};
+
+TrafficLight trafficLight;
+
+void setup() {
+}
+
+void loop() {
+  trafficLight.update(millis());
+}
